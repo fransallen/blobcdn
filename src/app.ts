9154574627a4b17f55014863bf5d70e081cba363
Frans Allen <fransallen@users.noreply.github.com>
2025-02-10 00:57:37 +0700

    added Blob-Version header

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import etag from 'etag';
 import { getBlob } from './blob';
 import { getInt } from './validator';
+import os from 'os';
 
 type BlobQuery = {
   seed?: string;
@@ -33,6 +34,10 @@ export async function getApp(req: Request, res: Response): Promise<void> {
       .header('Content-Type', 'image/svg+xml')
       .header('Cache-Control', 'public, max-age=0, must-revalidate')
       .header('ETag', etag(blob))
+      .header(
+        'Blob-Version',
+        `${process.env.VERSION ?? '1.0-dev'}-${os.hostname()}`,
+      )
       .send(blob);
   } catch (e) {
     console.error((e as Error).message);
