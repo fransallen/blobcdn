1435ae4e097f2046b0bdc85ccfa589eed3545275
Frans Allen <fransallen@users.noreply.github.com>
2025-02-16 13:37:04 +0700

    delete middleware

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,6 @@ import { getApp } from './app';
 const app = express();
 const port: number = parseInt(process.env.PORT || '3000', 10);
 
-// Allowed hostnames
-const allowedHosts = process.env.HOSTNAME
-  ? process.env.HOSTNAME.split(',').map((h) => h.trim())
-  : null; // If HOSTNAME is not set, do not apply the middleware
-
-if (allowedHosts) {
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    const host = req.hostname;
-    if (!allowedHosts.includes(host)) {
-      return res.status(403).contentType('text/plain').send('403 Forbidden');
-    }
-    next();
-  });
-}
-
 app.disable('x-powered-by');
 
 // Serve static files
