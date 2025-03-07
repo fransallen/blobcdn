6f79fe6ecff2a6cdc947e09a4ff8b9951e3bdd2a
Frans Allen <fransallen@users.noreply.github.com>
2025-03-07 13:54:07 +0700

    fly.io deployment

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,14 @@ app.disable('x-powered-by');
 // Serve static files
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Redirect blobcdn.fly.dev to blobcdn.com
+app.use((req: Request, res: Response, next: NextFunction) => {
+  if (req.hostname === 'blobcdn.fly.dev') {
+    return res.redirect(301, 'https://blobcdn.com');
+  }
+  next();
+});
+
 // Root route
 app.get('/', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../views', 'index.html'));
